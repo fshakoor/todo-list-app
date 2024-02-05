653b12d5166236217e25ded4065eaa2f8480da92
Faiz Shakoor <121912022+fshakoor@users.noreply.github.com>
2024-02-04 19:46:00 -0600

    Set up basic design and layout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import './style.css';
 
+// TODO: complete all code in one JS function
+// TODO: make a header
+// TODO: make a sidebar
+// TODO: make a main body
+// TODO: seperate and export into modules
+
 function component() {
-    const element = document.createElement('div');
-    element.innerHTML = 'Completed Setup';
+    const main = document.querySelector('.main');
+
+    const header = document.createElement('div');
+    const headerText = document.createElement('div');
+    headerText.innerText = "To-Do"
+    headerText.classList.add('headerText');
+    header.classList.add('header');
+
+    const sidebar = document.createElement('div');
+    sidebar.classList.add('sidebar');
+    const content = document.createElement('div');
+    content.classList.add('content');
+    
+    header.appendChild(headerText);
+    main.appendChild(header);
+    main.appendChild(sidebar);
+    main.appendChild(content);
+}
  
-    return element;
-  }
- 
-document.body.appendChild(component());
+component();
